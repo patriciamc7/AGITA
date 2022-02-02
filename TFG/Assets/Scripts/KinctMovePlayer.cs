b2efa82620ba389500c33d28ab946d8d5b229d95
sophie <romata1966@gmail.com>
2022-02-02 14:15:46 +0100

    moviment dreta

--- TFG/Assets/Scripts/KinctMovePlayer.cs
+++ TFG/Assets/Scripts/KinctMovePlayer.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class KinctMovePlayer : MonoBehaviour
 {
     public GameObject Hada;
-    public GameObject KinectParent; 
-    public GameObject Body;
-    public GameObject rightHand; 
-    public GameObject neck; 
-    public GameObject elbowRight; 
-    public GameObject sholderRight;
+    public GameObject KinectParent;
+    private GameObject Body;
+    private GameObject rightHand;
+    private GameObject neck;
+    private GameObject elbowRight;
+    private GameObject sholderRight;
     private Vector3 HandToNeck; 
-    private Vector3 HandToElbow; 
+    private Vector3 HandToElbow;
     private Vector3 HandToSholder;
     private int methodChoose = 1;
-    public Vector3 VectorInPlain  = new Vector3(0,0,2.92f);
-    private Vector3 posPlayersum; 
+    private Vector3 VectorInPlain  = new Vector3(0,0,2.92f);
+    private Vector3 posPlayersum;
+    public float vecloctyPlayer; 
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +27,16 @@ public class KinctMovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+       
+
         //If Body detected assign Kineckt gameobject
         if (GameObject.Find("Body_Person") != null)
         {
             Body = GameObject.Find("Body_Person"); 
             Body.gameObject.transform.SetParent(KinectParent.gameObject.transform);
+            //move player camioneta
+            Body.gameObject.transform.position = Body.gameObject.transform.position + new Vector3(-vecloctyPlayer * Time.deltaTime, 0, 0);
 
             rightHand = GetChildWithName(Body, "WristRight");
             neck = GetChildWithName(Body, "Neck");
@@ -40,7 +46,8 @@ public class KinctMovePlayer : MonoBehaviour
             HandToNeck = vector2nodesNormalice(rightHand.gameObject.transform.position, neck.gameObject.transform.position);
             HandToElbow = vector2nodesNormalice(rightHand.gameObject.transform.position, elbowRight.gameObject.transform.position);
             HandToSholder = vector2nodesNormalice(rightHand.gameObject.transform.position, sholderRight.gameObject.transform.position);
-            moveCharater(); 
+            moveCharater();
+            
         }
 
     }
@@ -106,12 +113,15 @@ public class KinctMovePlayer : MonoBehaviour
             VectorInPlain.x = HandToSholder.x;
             VectorInPlain.y = HandToSholder.y;
         }
+        //ejes al reves
+        VectorInPlain = rightHand.gameObject.transform.position;
+        VectorInPlain = Vector3.Scale(VectorInPlain, new Vector3(-1, 1, 1));
 
-        posPlayersum += VectorInPlain + new Vector3(5 * Time.deltaTime, 0, 0);  
-        posPlayersum.x = Hada.gameObject.transform.position.x  + (posPlayersum.x - Hada.gameObject.transform.position.x)/27;
-        posPlayersum.y = Hada.gameObject.transform.position.y  + (posPlayersum.y - Hada.gameObject.transform.position.y)/27;
-        posPlayersum.z = Hada.gameObject.transform.position.z;
-        Hada.gameObject.transform.position = posPlayersum; 
-    }
+        posPlayersum = Hada.gameObject.transform.position; 
+        posPlayersum.x = posPlayersum.x + (VectorInPlain.x - posPlayersum.x) / 27;
+		posPlayersum.y = posPlayersum.y + (VectorInPlain.y - posPlayersum.y) / 27;
+		posPlayersum.z = Hada.gameObject.transform.position.z;
+		Hada.gameObject.transform.position = posPlayersum;
+	}
     
 }
