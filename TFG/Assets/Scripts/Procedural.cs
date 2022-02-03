0f9a109c616fbb132d7fb2fad9f63129cce78ff5
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-02-03 22:20:55 +0100

    Extend envoirement

--- TFG/Assets/Scripts/Procedural.cs
+++ TFG/Assets/Scripts/Procedural.cs
@@ -6,12 +6,16 @@ public class Procedural : MonoBehaviour
 {
     public GameObject[] objects;
     public bool is_rot = true;
+    public bool is_left = false;
     private Quaternion rot = Quaternion.identity;
     // Start is called before the first frame update
     void Start()
     {
         if (is_rot)
             rot = Quaternion.Euler(Vector3.up * (Random.Range(0, 4) * 90));
+        else if (is_left)
+            rot = Quaternion.Euler(Vector3.up * 180);
+
         Instantiate(objects[Random.Range(0, objects.Length)], transform.position, rot);
 
     }
