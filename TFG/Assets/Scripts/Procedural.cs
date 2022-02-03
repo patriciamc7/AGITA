03c47b0168462b882f7165657bf1eff8b5e9551d
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-02-03 18:11:41 +0100

    Procedural ended more floor

--- TFG/Assets/Scripts/Procedural.cs
+++ TFG/Assets/Scripts/Procedural.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Procedural : MonoBehaviour
 {
     public GameObject[] objects;
-    [Range(0, 1)]
-    public float prob = 0.75f;
-    public bool prob_rot = true;
+    public bool is_rot = true;
     private Quaternion rot = Quaternion.identity;
     // Start is called before the first frame update
     void Start()
     {
-        if(Random.Range(0f,1f)<= prob)
-        {
-            if (prob_rot)
-                rot = Quaternion.Euler(Vector3.up * (Random.Range(0, 4) * 90));
-            Instantiate(objects[Random.Range(0, objects.Length)], transform.position, rot);
+        if (is_rot)
+            rot = Quaternion.Euler(Vector3.up * (Random.Range(0, 4) * 90));
+        Instantiate(objects[Random.Range(0, objects.Length)], transform.position, rot);
 
-        }
     }
 
  
