f85b4f15e1787b1608ee6451d7ee37305e871bf2
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-02-11 23:19:27 +0100

    chest particles

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -8,16 +8,15 @@ public class Events : MonoBehaviour
     {
         Transform trans = other.transform;
         Transform child = trans.Find("Chest");
+        ParticleSystem Fireflies = other.gameObject.GetComponentInChildren<ParticleSystem>();
         if (child != null)
         {
             Animator anim = child.GetComponent<Animator>();
             if (anim != null)
-            {
                 anim.Play("Animated PBR Chest _Opening_UnCommon");
-
-            }
+            if (Fireflies != null)
+                Fireflies.Play();
         }
-
     }
 
 }
