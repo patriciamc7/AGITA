f09977737f61bfc33159a16a5911cbf35bb2fe96
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-02-11 21:47:11 +0100

    Chest animation while player is on

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -6,26 +6,18 @@ public class Events : MonoBehaviour
 {
     public void OnTriggerEnter(Collider other)
     {
-        if (FindName(other.gameObject, "Chest"))
+        Transform trans = other.transform;
+        Transform child = trans.Find("Chest");
+        if (child != null)
         {
-            Transform trans = other.transform;
-            Transform child = trans.FindChild(name);
-            Animator anim = child.gameObject.GetComponent<Animator>();
-            /*if (anim != null)
-                anim.Play("Animated PBR Chest _Opening_UnCommon");*/
-            Debug.Log("True");
+            Animator anim = child.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.Play("Animated PBR Chest _Opening_UnCommon");
 
+            }
         }
 
     }
 
-    private bool FindName(GameObject obj, string name)
-    {
-        Transform trans = obj.transform;
-        Transform child = trans.FindChild(name);
-        if (child)
-            return true;
-        else
-            return false;
-    }
 }
