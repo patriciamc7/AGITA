8b31e63482eac8136a2b8a0d94208053a4a7dfc6
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-02-24 21:37:02 +0100

    test

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Events : MonoBehaviour
 {
+    public Material material;
+    public Vector3 scale;
     public void OnTriggerEnter(Collider other)
     {
         //Chest event
@@ -12,6 +14,9 @@ public class Events : MonoBehaviour
             Transform trans = other.transform;
             Transform child = trans.Find("Chest");
             ParticleSystem fireflies = other.gameObject.GetComponentInChildren<ParticleSystem>();
+
+            Transform child2 = child.transform.Find("Base");
+            ParticleSystem light = child2.gameObject.GetComponentInChildren<ParticleSystem>();
             if (child != null)
             {
                 Animator anim = child.GetComponent<Animator>();
@@ -19,6 +24,9 @@ public class Events : MonoBehaviour
                     anim.Play("Animated PBR Chest _Opening_UnCommon");
                 if (fireflies != null)
                     fireflies.Play();
+                if (light != null)
+                    StartCoroutine(DelayedAnimation(light, 0.9f));
+
             }
         }
         //Fountain event
@@ -26,14 +34,53 @@ public class Events : MonoBehaviour
         {
             Transform trans = other.transform;
             Transform child = trans.Find("Fountain");
-            ParticleSystem water = other.gameObject.GetComponentInChildren<ParticleSystem>();
+            ParticleSystem[] water = other.gameObject.GetComponentsInChildren<ParticleSystem>();
             if (child != null)
             {
-                if (water != null)
-                    water.Play();
+                for (int i = 0; i < water.Length; i++)
+                {
+                    water[i].Play();
+                }
+
             }
         }
-        
+
+        //Crystal event
+        if (other.gameObject.CompareTag("FloorCrystal"))
+        {
+            Transform trans = other.transform;
+            Transform child = trans.Find("Cristal");
+
+            if (child != null)
+            {
+                Transform mesh = this.gameObject.transform.Find("mesh");
+                if (mesh != null)
+                    mesh.gameObject.GetComponent<Renderer>().material = material;
+
+            }
+        }
+
+        //Mushroom event
+        if (other.gameObject.CompareTag("FloorMushroom"))
+        {
+            Transform trans = other.transform;
+            Transform child = trans.Find("Bolets");
+
+            if (child != null)
+            {
+                child.gameObject.transform.localScale = new Vector3(scale.x *Time.deltaTime , scale.y *Time.deltaTime , scale.z *Time.deltaTime );
+                //Animator anim = child.GetComponent<Animator>();
+                //if (anim != null)
+                //    anim.Play("Animated PBR Chest _Opening_UnCommon");
+
+            }
+        }
     }
 
+    // The delay coroutine
+    public IEnumerator DelayedAnimation(ParticleSystem ps, float time)
+    {
+        yield return new WaitForSeconds(time);
+        ps.Play();
+    }
 }
