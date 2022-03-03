62f191d0c13f790708661cc2b64b1b569369a1d6
sophie <romata1966@gmail.com>
2022-03-03 12:14:16 +0100

    Marge

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float cameraVelocity;
+    //public float cameraVelocity;
     public float totalDuration;
-
+    public KinctMovePlayer KinectScript;
+    public GameObject RightSide; 
+    public GameObject LeftSide;
+    //public boo
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,19 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time < totalDuration)
-            transform.Translate(cameraVelocity * Time.deltaTime, 0, 0);
+        // if (Time.time < totalDuration)
+        if (GameObject.Find("Body_Person") != null)
+        {
+            if (this.name == "RightCamera")
+                transform.Translate(-KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
+            else
+            {
+                transform.Translate(KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
+                RightSide.gameObject.transform.position += new Vector3(KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
+                LeftSide.gameObject.transform.position += new Vector3(KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
+
+            }
+        }
     }
+	
 }
