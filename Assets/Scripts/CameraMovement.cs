33cb28c0e52751c3c5ff14fdfee23319e1c5c226
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-03-04 22:20:05 +0100

    scale interior modules

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    //public float cameraVelocity;
     public float totalDuration;
     public KinctMovePlayer KinectScript;
     public GameObject RightSide; 
     public GameObject LeftSide;
-    //public boo
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Time.time < totalDuration)
-        if (GameObject.Find("Body_Person") != null)
-        {
+       // if (GameObject.Find("Body_Person") != null)
+        //{
             if (this.name == "RightCamera")
                 transform.Translate(-KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
             else
@@ -31,7 +23,7 @@ public class CameraMovement : MonoBehaviour
                 LeftSide.gameObject.transform.position += new Vector3(KinectScript.vecloctyPlayer * Time.deltaTime, 0, 0);
 
             }
-        }
+        //}
     }
 	
 }
