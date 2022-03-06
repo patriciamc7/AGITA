2526bb3075e3e35c82361c68f701201f52ad2b04
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-03-06 14:35:06 +0100

    procedural world

---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    public int numElements;
+    public int depth;
+    public GameObject[] modules;
+    // Start is called before the first frame update
+    void Start()
+    {
+       for (int i = 0; i < numElements; i++)
+            {
+                for (int j = 0; j < depth; j++)
+                {
+                    Vector3 rightPosition = new Vector3(2 * i, 0, -1-2*j);
+                    Instantiate(modules[j], rightPosition, Quaternion.identity);
+                    Vector3 leftPosition = new Vector3(2 * i, 0, 1+2*j);
+                    Instantiate(modules[j], leftPosition, Quaternion.Euler(Vector3.up * 180));
+                }
+            }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
