53783e354bb1d9e87d01fbc7c99b93e20e12106b
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-03-09 16:17:42 +0100

    Lake left side

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -13,89 +13,78 @@ public class Generation : MonoBehaviour
 
     void Start()
     {
-       for (int i = 0; i < numElements; i++)
+        GameObject left = this.transform.GetChild(0).gameObject;
+        GameObject right = this.transform.GetChild(1).gameObject;
+
+        for (int i = 0; i < numElements; i++)
        {
             for (int j = 0; j < depth; j++)
             {
                 Vector3 rightPosition = new Vector3(2 * i, 0, -1 - 2 * j);
                 Vector3 leftPosition = new Vector3(2 * i, 0, 1 + 2 * j);
 
-                //modules[j].GetComponent<Procedural>().isLeft = false;
+                modules[j].GetComponent<Procedural>().isLeft = false;
                 GameObject rightDepth = Instantiate(modules[j], rightPosition, Quaternion.identity);
-                rightDepth.transform.SetParent(this.transform);
-                
+                rightDepth.transform.SetParent(right.transform);
+                modules[j].GetComponent<Procedural>().isLeft = true;
+                GameObject leftObject = Instantiate(modules[j], leftPosition, Quaternion.identity);
+                leftObject.transform.SetParent(left.transform);
+
             }
-        //modules[j].GetComponent<Procedural>().isLeft = true;
-        //GameObject leftObject = Instantiate(modules[j], leftPosition, Quaternion.identity);
-        //this.transform.SetParent(leftObject.transform);
-        //if (isLake)
-        //{
-
-        //    lakeElement = i;
-        //    lakeDepth = j;
-        //    j += 2;
-        //    i += 3;
-        //    //for (int k = 0; k < LakeModules.Length; k++)
-        //    //{
-        //    //    GameObject rightObject  = Instantiate(LakeModules[k], rightPosition, Quaternion.identity);
-
-        //    //}
-        //}
-    
-
         }
         
-
     }
     void Update()
     {
         if(!oneTime)
             LakeGeneration();
-        
+       
     }
 
     public void LakeGeneration()
     {
         int numChilds = this.transform.GetChildCount();
         int count = 0;
-
-        for (int i = 0; i < numChilds; i++)
+        for (int l = 0; l < numChilds; l++)
         {
-            count = 0;
-            Transform childDepth = this.transform.GetChild(i);
-            if (childDepth)
+            Transform sideChild = this.transform.GetChild(l);
+            int sideNumChild = sideChild.transform.GetChildCount();
+
+            for (int i = 0; i < sideNumChild; i++)
             {
+                count = 0;
+                Transform childDepth = sideChild.transform.GetChild(i);
                 GameObject childModule = childDepth.transform.GetChild(0).gameObject;
-                if (childModule)
+                if (childModule.CompareTag("FloorLake"))
                 {
-                    if (childModule.CompareTag("FloorLake"))
+
+                    if (i + depth * 3 >= numElements)
                     {
                         for (int j = 0; j < depth * 3; j++)
                         {
                             if (j == 0 || j == 1 || j == 4 || j == 5 || j == 8 || j == 9)
                             {
-                                if (i + depth * 3 >= numElements)
-                                {
-                                    GameObject child1 = this.transform.GetChild(i + j).gameObject;
-                                    GameObject childFloor = child1.transform.GetChild(0).gameObject;
-                                    Vector3 position = childFloor.transform.position;
-                                    Destroy(childFloor);
-                                    GameObject Lake1 = Instantiate(LakeModules[count], position, Quaternion.identity);
-                                    Lake1.transform.SetParent(child1.transform);
-                                    count += 1;
-                                }
-                                else
-                                {
-                                    Vector3 position = childDepth.transform.position;
-                                    Destroy(childDepth.gameObject);
-                                    GameObject rightDepth = Instantiate(FirstDepthtWithoutLake, position, Quaternion.identity);
-                                }
 
+                                GameObject child1 = sideChild.transform.GetChild(i + j).gameObject;
+                                GameObject childFloor = child1.transform.GetChild(0).gameObject;
+                                Vector3 position = childFloor.transform.position;
+                                Destroy(childFloor);
+                                GameObject Lake1 = Instantiate(LakeModules[count], position, Quaternion.identity);
+                                Lake1.transform.SetParent(child1.transform);
+                                count += 1;
 
                             }
                         }
                         i += depth * 3 + 1;
                     }
+                    else
+                    {
+                        Vector3 position = childDepth.transform.position;
+                        Destroy(childDepth.gameObject);
+                        GameObject noLakeDepth = Instantiate(FirstDepthtWithoutLake, position, Quaternion.identity);
+                        noLakeDepth.transform.SetParent(sideChild.transform);
+                    }
+
                 }
             }
 
