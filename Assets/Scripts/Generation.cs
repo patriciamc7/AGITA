7b0b7ff4fc06d995a523c51dc0acf264a3748859
Patri <patricia.martinez.cruz.pmn@gmail.com>
2022-03-27 21:51:01 +0200

    Lake algorithm

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Generation : MonoBehaviour
@@ -9,7 +7,7 @@ public class Generation : MonoBehaviour
     public GameObject[] modules;
     public GameObject LakeModule;
     public GameObject FirstDepthtWithoutLake;
-    public bool oneTime = false;
+    private bool oneTime = false;
 
     void Start()
     {
@@ -17,11 +15,11 @@ public class Generation : MonoBehaviour
         GameObject right = this.transform.GetChild(1).gameObject;
 
         for (int i = 0; i < numElements; i++)
-       {
+        {
             for (int j = 0; j < depth; j++)
             {
-                Vector3 rightPosition = new Vector3(2 * i, 0, -1 - 2 * j);
-                Vector3 leftPosition = new Vector3(2 * i, 0, 1 + 2 * j);
+                Vector3 rightPosition = new Vector3(2*i, 0, -1-2*j);
+                Vector3 leftPosition = new Vector3(2*i, 0, 1+2*j);
 
                 modules[j].GetComponent<Procedural>().isLeft = false;
                 GameObject rightDepth = Instantiate(modules[j], rightPosition, Quaternion.identity);
@@ -32,68 +30,93 @@ public class Generation : MonoBehaviour
 
             }
         }
-        
     }
     void Update()
     {
-        if(!oneTime)
+        if (!oneTime)
             noRepetition();
     }
 
-    public void overwriteFloor(Transform depth, Transform side)
+    public void overwriteFloor(Transform depth, Transform side, Transform SecondDepth = null)
     {
+        string tag = depth.GetChild(0).tag;
         Vector3 position = depth.transform.position;
         Quaternion rotation = Quaternion.identity;
+
+        Destroy(depth.GetChild(0).gameObject);
         Destroy(depth.gameObject);
+
         if (side.name.Equals("Left"))
         {
             rotation = Quaternion.Euler(Vector3.up * 180);
         }
-        GameObject noRepetitionModule= Instantiate(FirstDepthtWithoutLake, position, rotation);
+
+        GameObject noRepetitionModule = Instantiate(FirstDepthtWithoutLake, position, Quaternion.identity);
+        
+        //Debug.Log(tag);
+        //Debug.Log(noRepetitionModule.transform.GetChildCount());
+
+        //&& noRepetitionModule.transform.GetChild(0).CompareTag(SecondDepth.gameObject.tag)
+        //   && !noRepetitionModule.transform.GetChild(0).CompareTag("Floor"))
+
+        //while (noRepetitionModule.transform.GetChild(0).CompareTag(tag))
+        //{
+        //    noRepetitionModule = Instantiate(FirstDepthtWithoutLake, position, Quaternion.identity);
+        //    Debug.Log(noRepetitionModule.tag);
+        //}
+        noRepetitionModule.transform.rotation = rotation;
         noRepetitionModule.name = "NoRepetition";
         noRepetitionModule.transform.SetParent(side.transform);
     }
     public int LakeGeneration(Transform depthTransform, Transform side, int i)
     {
+        int count0 = 0;
+        int count1 = 1;
         if (i + depth * 3 <= numElements)
         {
-            for (int j = 0; j < depth * 3; j++)
+            for (int j = 0; j < 3; j++)
             {
-                if (j == 0 || j == 1 || j == 4 || j == 5 || j == 8 || j == 9)
+               
+                GameObject childDepths0 = side.transform.GetChild(i + count0).gameObject;
+                GameObject childDepths1 = side.transform.GetChild(i + count1).gameObject;
+                GameObject childFloor0 = childDepths0.transform.GetChild(0).gameObject;
+                GameObject childFloor1 = childDepths1.transform.GetChild(0).gameObject;
+
+                if (j == 2)
                 {
-
-                    GameObject childDepths = side.transform.GetChild(i + j).gameObject;
-                    GameObject childFloor = childDepths.transform.GetChild(0).gameObject;
-
-                    if (j == 9)
+                    Vector3 position = childFloor1.transform.position;
+                    Quaternion rotation = Quaternion.identity;
+                    if (side.name.Equals("Left"))
                     {
-                        Vector3 position = childFloor.transform.position;
-                        Quaternion rotation = Quaternion.identity;
-                        if (side.name.Equals("Left"))
-                        {
-                            rotation = Quaternion.Euler(Vector3.up * 180);
-                            position += new Vector3(-4, 0, 0);
-                        }
-                        GameObject Lake1 = Instantiate(LakeModule, position, rotation);
-                        Lake1.transform.SetParent(childDepths.transform);
+                        rotation = Quaternion.Euler(Vector3.up * 180);
+                        position += new Vector3(-4, 0, 0);
                     }
-                    Destroy(childFloor);
-
+                    GameObject Lake = Instantiate(LakeModule, position, rotation);
+                    Lake.transform.SetParent(childDepths1.transform);
                 }
+                Destroy(childFloor0);
+                Destroy(childFloor1);
+                count0 += depth;
+                count1 += depth;
             }
             i += depth * 3;
             if (side.transform.GetChild(i).transform.GetChild(0).CompareTag("FloorLake"))
+            {
                 overwriteFloor(depthTransform, side);
+                Debug.Log(i);
+            }
         }
         else
             overwriteFloor(depthTransform, side);
+
+        
         return i;
     }
     public void noRepetition()
     {
-       int numChilds = this.transform.GetChildCount();
-       for (int l = 0; l < numChilds; l++)
-       {
+        int numChilds = this.transform.GetChildCount();
+        for (int l = 0; l < numChilds; l++)
+        {
             Transform sideChild = this.transform.GetChild(l);
             int sideNumChild = sideChild.transform.GetChildCount();
 
@@ -103,13 +126,19 @@ public class Generation : MonoBehaviour
                 GameObject childModule = childDepth.transform.GetChild(0).gameObject;
                 if (childModule.CompareTag("FloorLake"))
                     i = LakeGeneration(childDepth, sideChild, i);
-                //else if (i + depth < sideNumChild && childDepth.CompareTag("FirstDepth") && childModule.tag.Equals(sideChild.transform.GetChild(i + depth).GetChild(0).tag))
-                //    overwriteFloor(sideChild.transform.GetChild(i + depth), sideChild,false);
+                else if (i + depth < sideNumChild
+                    && childDepth.CompareTag("FirstDepth")
+                    && !childModule.CompareTag("Floor")
+                    && childModule.tag.Equals(sideChild.transform.GetChild(i + depth).GetChild(0).tag))
+                {
+                    overwriteFloor(sideChild.transform.GetChild(i + depth), sideChild, sideChild.transform.GetChild(i + 2 * depth));
+
+                }
             }
 
         }
         oneTime = true;
     }
-    
+
 }
 
--- Assets/Scripts/Procedural.cs
+++ Assets/Scripts/Procedural.cs
@@ -8,15 +8,27 @@ public class Procedural : MonoBehaviour
     public bool isRot = false;
     public bool isLeft = false;
     private Quaternion rot = Quaternion.identity;
-
+    private GameObject floor;
+    //private Camera LeftCamera;
+    //private Camera RightCamera;
+    //private float widthThresold = 3f;
     void Start()
     {
         if (isRot)
             rot = Quaternion.Euler(Vector3.up * (Random.Range(0, 4) * 90));
         if (isLeft)
             rot = Quaternion.Euler(Vector3.up * 180);
-        GameObject floor = Instantiate(objects[Random.Range(0, objects.Length)], transform.position, rot);
+        floor = Instantiate(objects[Random.Range(0, objects.Length)], transform.position, rot);
         floor.transform.SetParent(this.transform);
 
     }
+    
+    //void Update()
+    //{
+    //    Vector2 screenPositionL = LeftCamera.WorldToScreenPoint(transform.position);
+    //    Vector2 screenPositionR = RightCamera.WorldToScreenPoint(transform.position);
+    //    if (screenPositionL.x < widthThresold && screenPositionR.x < widthThresold)
+    //        Destroy(floor);
+    //}
+
 }
